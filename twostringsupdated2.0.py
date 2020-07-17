a68f1e87617d0d57b910ecbf5a3c4af3332141f0
jiaxiang2003 <61258340+jiaxiang2003@users.noreply.github.com>
2020-07-17 22:27:26 +0800

    Update twostringsupdated2.0.py

--- twostringsupdated2.0.py
+++ twostringsupdated2.0.py
@@ -22,11 +22,11 @@ while(t>0): # to know that it hasn't finished all the cases
     a = sorted(s1) # sort the strings
     b = sorted(s2) # sort the strings
     if (len(s1)!=len(s2)): # if length of strings doesn't equal to each other
-        print("No") # what to do if so
+        print("NO") # what to do if so
         break # stop the loop
     else: # if length of strings equal to each other
         if a == b: 
-            print("yes")
+            print("YES")
         else:
-            print("no")
+            print("NO")
     t =t-1 #-1 because 1 case is done so deduct from the total 
