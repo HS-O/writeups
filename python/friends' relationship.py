98eb8680bdc62382216868538235ca8a5682cfe0
HS-O <61258296+HS-O@users.noreply.github.com>
2020-07-18 23:07:17 +0800

    Create friends' relationship.py

---
+++ python/friends' relationship.py
@@ -0,0 +1,36 @@
+'''
+Question link: friends' relationship 
+https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/friends-relationship-1/
+
+Question description:
+
+Jack is your friend and Sophia is his girlfriend. But due to some unknown reason ( unknown for us, they know it) Sophia started hating to Jack. 
+
+Now, Jack is in big trouble but he has a solution, He knows that if he will gift T patterns of a particular type ( For pattern observation see the sample test cases) then Sophia will start loving again to Jack. 
+
+But Jack is depressed now and need your help to gift her that type patterns of '*' and '#' of N lines. So, it's your responsibility to save your friend's relationship.
+
+Example: 
+
+input : 2, 3, 4 
+
+output: 
+
+*####*
+**##**
+******
+
+*######*
+**####**
+***##***
+********
+
+Question solution:
+'''
+
+T = int(input())
+for i in range(T):
+    N = int(input())
+    for m in range(1, N+1):
+        print("*" * m + "#" * (2*N - 2*m) + "*" * m)
+    i = i + 1
